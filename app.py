d569a09130ae51690901424276b3d7863b7707db
vincmartel <43604469+vincmartel@users.noreply.github.com>
2024-04-17 12:11:42 +0200

    Update app.py projet 8

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pickle
 import json
 from flask import Flask, jsonify, request
 import pandas as pd
+import shap
 
 # Création d'une instance de l’application Flask
 app = Flask(__name__)
@@ -24,7 +25,11 @@ with open('scaler.pkl', 'rb') as scaler:
 # Importer le modèle
 with open('lgbm_clf.pkl', 'rb') as lgbm:
     loaded_model = pickle.load(lgbm)
-    
+
+# Vérification démarrage API
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'API Flask en cours d\'exécution !'})
 
 # Définition d'une route pour l'API à l'adresse /prediction
 @app.route('/prediction', methods=['POST'])
@@ -48,6 +53,10 @@ def prediction():
 
     # Prediction de la probabilité d’appartenance du prêt à la classe 1 (c’est-à-dire la probabilité de difficulté de paiement)
     proba=loaded_model.predict_proba(pret_array_std)[0][1]
+
+    # Calculer les valeurs SHAP pour le prêt sélectionné
+    explainer = shap.TreeExplainer(loaded_model)
+    shap_values = explainer.shap_values(pret_array_std)
     
     # Décision de pret avec seuil de probabilité de défaut à 42%
     y_pred = (proba >= 0.42).astype(int)
@@ -57,16 +66,30 @@ def prediction():
         classe = "prêt accordé"
     else:
         classe = "cas d'erreur"
+
+    # Shap value pour la classe 1 et transformation en liste
+    #shap_local=shap_values[1][0].tolist()
     
     
     # Envoi de la réponse JSON contenant la probabilité de défaut (pred) et la décision concernant le prêt.
     try:
         return jsonify({'pred': proba,
-                        'decision': classe})
+                        'decision': classe,
+                        'shap_local': shap_values[1][0].tolist(),  # Shap value pour la classe 1 et transformation en liste
+                        'features': pret.columns.tolist(),
+                        'values': pret.values[0].tolist()
+                        #'shap_values': shap_values
+                        #'feature_names': pret.columns.tolist(),
+                        #'feature_values': pret.values[0].tolist()
+                       }
+                      )
     
     except Exception as e:
         return jsonify({'error': str(e)})
 
+# Si le script est exécuté directement, l’application Flask est lancée en mode débogage sur le port 8000    
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
 # Si le script est exécuté directement, l’application Flask est lancée en mode débogage sur le port 8000    
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
